fb128da6ac94763ac1e3a0ad7f5640e347618abf
vadinci <info@vadinci.net>
2021-01-30 11:24:01 +0100

    blah

--- Unity/GGJ2021_Unity/Assets/GGJ2021/Scripts/UI/ScoreLabel.cs
+++ Unity/GGJ2021_Unity/Assets/GGJ2021/Scripts/UI/ScoreLabel.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
 
 public class ScoreLabel : MonoBehaviour
 {
@@ -14,17 +15,33 @@ public class ScoreLabel : MonoBehaviour
 
         text.text = ScoreManager.Score.ToString();
 
+        // register listeners
         ScoreManager.OnScoreChanged += OnScoreChanged;
+        ScoreManager.OnScoreIncreased += OnScoreIncread;
+        ScoreManager.OnScoreDecreased += OnScoreDecreased;
     }
 
     private void OnDestroy()
     {
+        // unregister listeners
         ScoreManager.OnScoreChanged -= OnScoreChanged;
+        ScoreManager.OnScoreIncreased -= OnScoreIncread;
+        ScoreManager.OnScoreDecreased -= OnScoreDecreased;
     }
 
     // Update is called once per frame
-    public void OnScoreChanged(ScoreChangeEvent e) 
+    public void OnScoreChanged(ScoreChangeEvent e)
     {
         text.text = e.NewScore.ToString();
+    }
+
+    private void OnScoreDecreased(ScoreChangeEvent e)
+    {
+        // @TODO: some 'negative' animation
+    }
+
+    private void OnScoreIncread(ScoreChangeEvent e)
+    {
+        // @TODO: some 'positive' animation
     }
 }
