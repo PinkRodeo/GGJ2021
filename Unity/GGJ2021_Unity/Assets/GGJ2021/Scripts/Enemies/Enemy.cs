c4c3f4f455f67028e03e904c6772f975c4f9372c
vadinci <info@vadinci.net>
2021-01-30 13:09:36 +0100

    basic enemy 'class' and OrbitBehavior

---
+++ Unity/GGJ2021_Unity/Assets/GGJ2021/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject other = collision.gameObject;
+
+        Debug.Log("Check if collision is with player, and send some event if so?");
+    }
+}
