4b64287c6b108c1543971b0651733c4355930963
Razedd <poll.jonker@hotmail.com>
2021-01-30 23:19:58 +0100

    speed and focus application, key controls

--- Unity/GGJ2021_Unity/Assets/GGJ2021/Scripts/PlayerMovement/PlayerController.cs
+++ Unity/GGJ2021_Unity/Assets/GGJ2021/Scripts/PlayerMovement/PlayerController.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private bool _movingDown = true;
+    private bool keyboardenabled = false;
     [SerializeField]
     private float ForceIntensity = 10;
     [SerializeField]
@@ -23,37 +24,65 @@ public class PlayerController : MonoBehaviour
     {
         mousesaved = Input.mousePosition;
         _rb = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        //Vector3 mouseRotRaw = mousesaved - new Vector3(Screen.width /2 , Screen.height/2 , 0);
-        Vector3 mouseNorm = new Vector3(mousesaved.x / Screen.width, mousesaved.y / Screen.height);
-        Vector3 mousecurve = new Vector3(steering.Evaluate(mouseNorm.x), steering.Evaluate(mouseNorm.y), 0);
-        //Vector3 norm = new Vector3(mousecurve.x -1, mousecurve.y -1, mousecurve.z -1 );
-        Vector3 mouseRotNorm = mousecurve;
-        transform.Rotate(new Vector3(-mouseRotNorm.y * RotSpeed, mouseRotNorm.x * RotSpeed, 0));
-        //transform.eulerAngles = new Vector3(transform.eulerAngles.x, 0, transform.rotation.eulerAngles.z);
-
-
-        if (mousesaved != Input.mousePosition)   // set the mouse saved positon
+        if (Input.GetKey(KeyCode.Y))
         {
-            mouseoffset = Input.mousePosition - mousesaved;
-            mousesaved = Input.mousePosition;
-        }
-        else
-        {
-            mouseoffset = mouseoffset * 0.6f; // when not moving mouse, decrease stear ?? will be normalized so doesnt do anything?
-        }
+            _movingDown = false;
+            keyboardenabled = true;
+            _rb.velocity = Vector3.zero;
 
+            
+
+
+        }
+        if (keyboardenabled)
+        {
+            if (Input.GetKey(KeyCode.W))
+                transform.Rotate(-Vector3.left * 50 * Time.deltaTime);
+            if (Input.GetKey(KeyCode.A))
+                transform.Rotate(Vector3.up * 50 * Time.deltaTime);
+            if (Input.GetKey(KeyCode.S))
+                transform.Rotate(Vector3.left * 50 * Time.deltaTime);
+            if (Input.GetKey(KeyCode.D))
+                transform.Rotate(-Vector3.up * 50 * Time.deltaTime);
+                
+            _rb.velocity = transform.forward * MaxSpeed;
+        }
         if (_movingDown)
         {
-            //_rb.AddForce(transform.forward * ForceIntensity ,ForceMode.Force);
-            //_rb.velocity = new Vector3(Mathf.Clamp(_rb.velocity.x, -MaxSpeed, MaxSpeed), Mathf.Clamp(_rb.velocity.y, -MaxSpeed, MaxSpeed), Mathf.Clamp(_rb.velocity.z, -MaxSpeed, MaxSpeed));
-            _rb.velocity = transform.forward * MaxSpeed;
+            //Vector3 mouseRotRaw = mousesaved - new Vector3(Screen.width /2 , Screen.height/2 , 0);
+            Vector3 mouseNorm = new Vector3(mousesaved.x / Screen.width, mousesaved.y / Screen.height);
+            Vector3 mousecurve = new Vector3(steering.Evaluate(mouseNorm.x), steering.Evaluate(mouseNorm.y), 0);
+            //Vector3 norm = new Vector3(mousecurve.x -1, mousecurve.y -1, mousecurve.z -1 );
+            Vector3 mouseRotNorm = mousecurve;
+            transform.Rotate(new Vector3(Mathf.Clamp(-mouseRotNorm.y * RotSpeed, 0, 180), mouseRotNorm.x * RotSpeed, 0));
+            
+            if (mousesaved != Input.mousePosition)   // set the mouse saved positon
+            {
+                mouseoffset = Input.mousePosition - mousesaved;
+                mousesaved = Input.mousePosition;
+            }
+            else
+            {
+                mouseoffset = mouseoffset * 0.6f; // when not moving mouse, decrease stear ?? will be normalized so doesnt do anything?
+            }
+
+             _rb.velocity = transform.forward * MaxSpeed;
             if (Input.GetMouseButton(0)) transform.Rotate(new Vector3(0, 0, 1));
             if (Input.GetMouseButton(1)) transform.Rotate(new Vector3(0, 0, -1));
         }
     }
+
+
+    void OnApplicationFocus(bool hasFocus)
+    {
+        _movingDown = hasFocus;
+        if (!hasFocus) _rb.velocity = Vector3.zero;
+    }
+
 }
